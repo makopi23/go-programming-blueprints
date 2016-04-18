05bf78014fccd3f9a736a6e836639b2c2d8f41bd
makopi23 <kipersonalpc@gmail.com>
2016-04-18 20:36:39 +0900

    Section1.1

---
+++ src/chapter01/chat/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+			w.Write([]byte(`
+					<html>
+						<head>
+							<title>チャット</title>
+						</head>
+						<body>
+							チャットしましょう
+						</body>
+					</html>
+					`))
+		})
+	// Webサーバを起動します
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
