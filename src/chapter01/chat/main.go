4465277039f6d211d7394dc58d6ce5b6a1918fa0
makopi23 <kipersonalpc@gmail.com>
2016-05-10 20:05:26 +0900

    section1.1 finished.

--- src/chapter01/chat/main.go
+++ src/chapter01/chat/main.go
@@ -3,21 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+	"text/template"
+	"path/filepath"
+	"sync"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-			w.Write([]byte(`
-					<html>
-						<head>
-							<title>チャット</title>
-						</head>
-						<body>
-							チャットしましょう
-						</body>
-					</html>
-					`))
+// templは１つのテンプレートを表します
+type templateHandler struct {
+	once		sync.Once
+	filename	string
+	templ		*template.Template
+}
+
+// ServerHTTPはHTTPリクエストを処理します
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	t.once.Do(func() {
+			t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 		})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 	// Webサーバを起動します
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
