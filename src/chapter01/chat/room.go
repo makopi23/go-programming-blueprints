bf8b5aed1dd15ef6d7bba51b43dd4e5578f9c788
makopi23 <kipersonalpc@gmail.com>
2016-06-07 00:30:03 +0900

    masterブランチ切り替え前にコミット

--- src/chapter01/chat/room.go
+++ src/chapter01/chat/room.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gorilla/websocket"
 	//"github.com/oreilly-japan/go-programming-blueprints/chapter1/trace"
+	"trace"
 )
 
 type room struct {
@@ -13,6 +14,7 @@ type room struct {
 	join chan *client
 	leave chan *client
 	clients map[*client]bool
+	tracer trace.Tracer
 }
 
 func newRoom() *room{
@@ -28,17 +30,27 @@ func (r *room) run() {
 	for {
 		select {
 			case client := <- r.join:
+				// 参加
 				r.clients[client] = true
+				t.tracer.Trace("新しいクライアントが参加しました")
 			case client := <- r.leave:
+				// 退室
 				delete(r.clients, client)
 				close (client.send)
+				r.tracer.Trace("クライアントが退室しました")
 			case msg := <- r.forward:
+				r.tracer.Trace("メッセージを受信しました： ", string(msg))
+				// すべてのクライアントにメッセージを転送
 				for client := range r.clients {
 					select {
 						case client.send <- msg:
+							// メッセージを送信
+							r.tracer.Trace(" -- クライアントに送信されました")
 						default:
+							// 送信に失敗
 							delete(r.clients, client)
 							close(client.send)
+							r.tracer.Trace(" -- 送信に失敗しました。クライアントをクリーンアップします")
 					}
 				}
 		}
